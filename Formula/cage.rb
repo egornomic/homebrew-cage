bbaa27408df9504969ccb4d7fa176e0d6a201714
egornomic <git@egornomic.xyz>
2026-01-23 20:52:55 +0100

    chore: bump cage to v0.3.1

--- Formula/cage.rb
+++ Formula/cage.rb
@@ -1,8 +1,8 @@
 class Cage < Formula
   desc "Opinionated devcontainer launcher with a ready-to-go template"
   homepage "https://github.com/egornomic/cage"
-  url "https://github.com/egornomic/cage/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "cef59d312f3d93c5aa75172a2375c1a5d461bf43dba6308460ead95cf112fcfe"
+  url "https://github.com/egornomic/cage/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "a715fb47abb41b2ad0939db8be80f800d02e7e1de473c9d7b82e22902171ad6f"
 
   def install
     bin.install "cage"
